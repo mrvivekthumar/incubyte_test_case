07930cccdc2d41850744bb4fee7e19bb4c4a5967
mrvivekthumar <mrvivekthumar@gmail.com>
2025-07-05 15:14:31 +0530

    Green: Implement minimal add method to pass empty string test
    - Added add(String) method returning 0
    - Test now passes
    - Ready for next TDD cycle

--- src/main/java/co/incubyte/stringcalculator/StringCalculator.java
+++ src/main/java/co/incubyte/stringcalculator/StringCalculator.java
@@ -1,5 +1,7 @@
 package co.incubyte.stringcalculator;
 
 public class StringCalculator {
-
+    public int add(String numbers) {
+        return 0; // Simplest implementation to make the test pass
+    }
 }
