0a9ed3bab7a0605b40c80ea9e3be1ca20a36f0a8
mrvivekthumar <mrvivekthumar@gmail.com>
2025-07-05 14:43:16 +0530

    first commit

---
+++ src/test/java/co/incubyte/test_case/TestCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package co.incubyte.test_case;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
